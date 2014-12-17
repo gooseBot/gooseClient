131c4c93f1f1f902a6f4430c3b0db4a63b3507f3
gooseBot <time2goup@gmail.com>
2014-12-16 21:09:42 -0800

    clean up, calc angle and distance for target

--- app/src/main/java/time2go/goosegun/gooseController/TargetRangeView.java
+++ app/src/main/java/time2go/goosegun/gooseController/TargetRangeView.java
@@ -11,12 +11,16 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
-public class SquareView extends View {
+public class TargetRangeView extends View {
 
     private Paint paint = new Paint();
-    private Path path = new Path();
+    private float eventX = 0 ;
+    private float eventY = 0;
+    private boolean drawTouchPointer=false;
+    private int scannerOriginX=0;
+    private int scannerOriginY=0;
 
-    public SquareView(Context context, AttributeSet attrs) {
+    public TargetRangeView(Context context, AttributeSet attrs) {
         super(context, attrs);
         paint.setAntiAlias(true);
         paint.setStrokeWidth(5f);
@@ -28,34 +32,47 @@ public class SquareView extends View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int size = Math.min(getMeasuredWidth(), getMeasuredHeight());
         setMeasuredDimension(size, size/2);
+        scannerOriginY=size/2;
+        scannerOriginX=size/2;
     }
     @Override
     protected void onDraw(Canvas canvas) {
-        canvas.drawPath(path, paint);
+        if (drawTouchPointer) canvas.drawCircle (eventX, eventY, 50, paint);
         canvas.drawCircle(canvas.getWidth()/2,canvas.getHeight(),canvas.getWidth()/2,paint);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        // Get the coordinates of the touch event.
-        float eventX = event.getX();
-        float eventY = event.getY();
-
-        TextView txt = (TextView) ((Activity)getContext()).findViewById(R.id.text1);
-        txt.setText(eventX + ", " + eventY);
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                // Set a new starting point
-                path.moveTo(eventX, eventY);
-                return true;
+                drawTouchPointer=true;
+                break;
             case MotionEvent.ACTION_MOVE:
-                // Connect the points
-                path.lineTo(eventX, eventY);
+                drawTouchPointer=true;
+                break;
+            case MotionEvent.ACTION_UP:
+                drawTouchPointer=false;
                 break;
             default:
                 return false;
         }
+
+        // Get the coordinates of the touch event.
+        eventX = event.getX();
+        eventY = event.getY();
+        if (eventY > this.getHeight()) eventY = this.getHeight();
+        if (eventY < 0) eventY=0;
+        if (eventX > this.getWidth()) eventX = this.getWidth();
+        if (eventX < 0) eventX=0;
+        int deltaX = scannerOriginX-(int)eventX;
+        int deltaY = scannerOriginY-(int)eventY;
+        double angleInDegrees = Math.atan2(deltaY, deltaX) * 180 / Math.PI;
+        double distance = Math.sqrt( Math.pow(deltaY,2) + Math.pow(deltaX,2));
+
+        TextView txt = (TextView) ((Activity)getContext()).findViewById(R.id.text1);
+        txt.setText("angle " + String.format("%.0f", angleInDegrees) + " distance " + String.format("%.0f", distance));
+
         // Makes our view repaint and call onDraw
         invalidate();
         return true;
